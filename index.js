b2f41f4540475a4821d5b672e9f159b00f7e5d25
ming <choimg51984@gmail.com>
2024-09-27 17:24:16 +0900

    python

--- index.js
+++ index.js
@@ -18,14 +18,35 @@ app.get('/', (req, res) => {
   res.send('Hello world https test completed');
 });
 
-// app.get('/get_tasks', async (req, res) => {
-//   try {
-//     const result = await database.query('SELECT * FROM task');
-//     return res.status(200).json(result.rows);
-//   } catch (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-// });
+app.post('/chat', (req, res) => {
+  const sendedQuestion = req.body.question;
+
+  // EC2 서버에서 현재 실행 중인 Node.js 파일의 절대 경로를 기준으로 설정합니다.
+  const scriptPath = path.join(__dirname, 'bizchat.py');
+  const pythonPath = path.join(__dirname, 'venv', 'bin', 'python3');
+
+  // Spawn the Python process with the correct argument
+  const result = spawn(pythonPath, [scriptPath, sendedQuestion]);
+
+  output = '';
+
+  //파이썬 파일 수행 결과를 받아온다.
+  result.stdout.on('data', function (data) {
+    output += data.toString();
+  });
+
+  result.on('close', (code) => {
+    if (code == 0) {
+      res.status(200).json({ answer: output });
+    } else {
+      res.status(500).send('Something went wrong');
+    }
+  });
+
+  result.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+});
 
 app.use(require('./routes/getRoutes'));
 app.use(require('./routes/postRoutes'));
